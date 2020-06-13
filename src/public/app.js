8a978a066e80a2d3301765071566c7c362464d2e
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-06-13 22:23:39 +0100

    get images

--- src/public/app.js
+++ src/public/app.js
@@ -1,1 +1,25 @@
-alert("hello")
+getAllImages();
+
+
+let col = document.querySelector("#imageHolder");
+
+let uiTemplate = imageKey => {
+    return (`
+    <div class="col-md-3" >
+<img src="https://instagram-app.s3.us-west-1.amazonaws.com/${imageKey}" alt="..." class="img-thumbnail cover my-3">
+                </div>`)
+}
+
+async function getAllImages() {
+    try {
+        let response = await axios.get('http://localhost:3000/upload');
+        let data = response.data.data.Contents;
+        data.forEach(image => {
+            $('#imageHolder').append(uiTemplate(image.Key));
+
+        });
+
+    } catch (err) {
+        console.log(err)
+    }
+}
