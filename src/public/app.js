b31ae04ad93de87afe7c0ad9e42e1df72eb5f7f5
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-06-13 23:01:38 +0100

    upload of image

--- src/public/app.js
+++ src/public/app.js
@@ -16,7 +16,7 @@ async function getAllImages() {
     loader.style.display = 'block';
     try {
         let response = await axios.get('http://localhost:3000/upload');
-        let data = response.data.data.Contents;
+        let data = response.data.data.Contents.reverse();
         data.forEach(image => {
             $('#imageHolder').append(uiTemplate(image.Key));
 
@@ -34,6 +34,21 @@ let form = document.querySelector("#uploadForm");
 let file = document.querySelector("#file")
 form.addEventListener('submit', e => {
     e.preventDefault();
-    let dat = file.files[0];
-    console.log(dat)
+    let data = file.files[0];
+    var formData = new FormData;
+    formData.append('image', data);
+    loader.style.display = 'block';
+
+    axios.post("http://localhost:3000/upload", formData).then(res => {
+        console.log(res)
+        loader.style.display = 'none';
+        alert("Image Added");
+        getAllImages();
+    }).catch(err => {
+        console.log(err)
+        loader.style.display = 'none';
+        alert("Something Went Wrong");
+
+    })
+
 })
