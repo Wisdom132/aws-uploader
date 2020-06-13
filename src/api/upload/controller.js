7f2306305f01c6b8490bb17a29df81d49ee9a444
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-06-13 19:10:45 +0100

    MRC pattern

---
+++ src/api/upload/controller.js
@@ -0,0 +1,22 @@
+const upload = require('../../config/aws')
+
+const singleUpload = upload.upload.single('image');
+
+
+exports.uploadImage = (req, res) => {
+    try {
+        singleUpload(req, res, err => {
+            if (err) {
+                return res.status(422).json({
+                    title: "File Upload Error",
+                    desription: err.message
+                })
+            }
+            return res.status(200).json({
+                imageURL: req.file.location
+            })
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
--- src/api/upload/routes.js
+++ src/api/upload/routes.js
@@ -1,32 +1,13 @@
 const uploadController = require("./controller");
 const router = require("express").Router();
-const upload = require('../../config/aws')
 
 
 
 //create upload instance
 
 
-const singleUpload = upload.upload.single('image');
 
-router.post("/", (req, res) => {
-    try {
-        singleUpload(req, res, err => {
-            if (err) {
-                return res.status(422).json({
-                    title: "File Upload Error",
-                    desription: err.message
-                })
-            }
-            return res.status(200).json({
-                imageURL: req.file.location
-            })
-        })
-    } catch (err) {
-        console.log(err)
-    }
-
-})
+router.post("/", uploadController.uploadImage)
 
 
 
